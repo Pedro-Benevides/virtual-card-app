90b6f1b53a20431fbbd63f8074230b5cea713f0a
Pedro <pvsbenevides197@gmail.com>
2023-03-26 22:33:09 -0300

    feat(web): adiciona endpoints para criação e busca de cards

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/generate', function () {
     return view('welcome');
+});
+
+Route::prefix('/person-cards')->group(function () {
+    Route::post('/', 'PersonCardController@create');
+
+    Route::prefix('/{uuid}')->group(function () {
+        Route::get('/', "PersonCardController@getByUuid");
+    });
 });
