fe1b6b89edc691040efabd69ad982c1ddbf42c3a
Pedro <pvsbenevides197@gmail.com>
2023-03-28 16:40:47 -0300

    chore(qr-coe.blade): adiciona opção de download a pagina

--- resources/views/livewire/qr-code.blade.php
+++ resources/views/livewire/qr-code.blade.php
@@ -1,12 +1,13 @@
 <div>
     <center>
         <h2>{{$card->person_name}}</h2>
-        <center>
-            <span>Here is your Qr-Code with the link to your virtual card, save in your phone!</span>
-        </center>
+        <div>
+            <span>Here is your Qr-code with the link to your virtual card, save in your phone!</span>
+        </div>
         <img src="{{ route('gen-code', ['cardUuid' => $card->uuid]) }}" width="20%" height="40%">
     </center>
     <center>
-        <button wire:click="exports">Baixar</button>
+        <button wire:click="exports">Download Qr-code</button>
+        <button onclick="window.print()">Download Page</button>
     </center>
 </div>
