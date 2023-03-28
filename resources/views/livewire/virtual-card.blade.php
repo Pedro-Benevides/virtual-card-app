1fbbfe6292411ea94088619440a70700575855d9
Pedro <pvsbenevides197@gmail.com>
2023-03-28 17:10:23 -0300

    chore(virtual-card.blade): substitui div por center

--- resources/views/livewire/virtual-card.blade.php
+++ resources/views/livewire/virtual-card.blade.php
@@ -1,20 +1,19 @@
 <div>
-    <h3>Hello my name is {{ $card->person_name }}</h3>
-
-    <div>
+    <center>
+        <h3>Hello my name is {{ $card->person_name }}</h3>
         <h2>My history</h2>
         <span>Lorem ipsum dolor sit amet. Eum nemo commodi ut autem laborum id cumque ratione nam ducimus asperiores</span>
-    </div>
+    </center>
 
     <br />
 
-    <div>
-        <a href="{{$card->linkedin_url}}">
-            <button>LinkedIn</button>
-        </a>
-
+    <center>
         <a href="{{$card->github_url}}">
             <button>GitHub</button>
         </a>
-    </div>
+
+        <a href="{{$card->linkedin_url}}">
+            <button>LinkedIn</button>
+        </a>
+    </center>
 </div>
